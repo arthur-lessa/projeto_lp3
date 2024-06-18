3276a13b5e74de571f78447c0e522cd606ed436f
Arthur Lessa <thurlessaf@gmail.com>
2024-06-18 11:30:54 -0300

    flask usando blocks

--- projeto_lp3/app.py
+++ projeto_lp3/app.py
@@ -1,22 +1,34 @@
-from flask import Flask
+from flask import Flask, render_template 
 
-app = Flask("Minha App")
+# template = arquivo HTML passado
+
+app = Flask("__name__")
 
 # rota + função
 
 # / - home page
 @app.route("/")
 def home():
-    return "<h1>Home Page</h1>"
+    return render_template("home.html")
 
 # /contato - página de contato
 @app.route("/contato")
 def contato():
-    return "<h1>Contato</h1>"
+    return render_template("contato.html")
 
 # /produtos - página com alguns produtos
 @app.route("/produtos")
 def produtos():
-    return "<h1>Produtos</h1>"
+    lista_produtos = [
+        {"nome": "Coca-cola", "descricao": "Mata a sede"},
+        {"nome": "Doritos", "descricao": "Suja a mão"},
+        {"nome": "Chocolate", "descricao": "Bom"}
+
+
+    ]
+
+    return render_template("produtos.html", produtos = lista_produtos)
+
+
 
 
