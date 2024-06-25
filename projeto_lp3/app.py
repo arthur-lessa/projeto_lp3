fe339e2604a2b4d4441e65b0dfb231c5b4b1e8dd
Arthur Lessa <thurlessaf@gmail.com>
2024-06-25 11:22:34 -0300

    métodos get e post para produtos

--- projeto_lp3/app.py
+++ projeto_lp3/app.py
@@ -1,10 +1,40 @@
-from flask import Flask, render_template 
+from flask import Flask, render_template, request 
+
+lista_produtos = [
+        {"nome": "Coca-cola", "descricao": "Mata a sede"},
+        {"nome": "Doritos", "descricao": "Suja a mão"},
+        {"nome": "Chocolate", "descricao": "Bom"}
+    ]
 
 # template = arquivo HTML passado
 
 app = Flask("__name__")
 
 # rota + função
+
+# GET /produtos/cadastro devolver o form
+@app.route("/produtos/cadastro")
+def cadastro_produto():
+    return render_template("cadastro_produto.html")
+
+# POST /produtos que vai lidar com os dados enviados pelo form
+# acessar o objeto request
+@app.route("/produtos", methods=['POST'])
+def salvar_produtos():
+    # pegando os valores digitados no form
+    # que estão no request    
+    nome = request.form["nome"]
+    descricao = request.form["descricao"]
+    
+    # crio um novo produto (novo dict)
+    produto = { "nome" : nome, "descricao" : descricao, "imagem" : "" }
+    
+    # adiciona o novo produto na lista
+    lista_produtos.append(produto)
+    
+    # devolvo o template COM o novo produto
+    return render_template("produtos.html", produtos = lista_produtos)
+
 
 # / - home page
 @app.route("/")
@@ -19,16 +49,6 @@ def contato():
 # /produtos - página com alguns produtos
 @app.route("/produtos")
 def produtos():
-    lista_produtos = [
-        {"nome": "Coca-cola", "descricao": "Mata a sede"},
-        {"nome": "Doritos", "descricao": "Suja a mão"},
-        {"nome": "Chocolate", "descricao": "Bom"}
-
-
-    ]
-
     return render_template("produtos.html", produtos = lista_produtos)
 
-
-
-
+    
